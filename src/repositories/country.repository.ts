924b6839cba715441e961c49460290e33d0e1be7
Arsenio Lourenco <43965372+ArsenioLourenco@users.noreply.github.com>
2021-10-14 10:42:31 +0100

    creat country repository

--- src/repositories/country.repository.ts
+++ src/repositories/country.repository.ts
@@ -2,6 +2,4 @@ import {EntityRepository, Repository} from 'typeorm';
 import {Countries} from '../models/Countries';
 
 @EntityRepository(Countries)
-export default class CountryRepository extends Repository<Countries>{
-
-}
+export default class CountryRepository extends Repository<Countries>{}
