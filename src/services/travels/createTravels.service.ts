fa486500a834caf3b2766e24568b94d089fec541
Rosa Chicato <rosacristinachicafo@gmail.com>
2021-10-18 13:01:49 +0100

    :construction:construino o crud de viagens

---
+++ src/services/travels/createTravels.service.ts
@@ -0,0 +1,15 @@
+
+
+
+
+interface IcreateTravels{
+    sportId: number,
+    origin: number,
+    destiny: number,
+    depatureDate: Date,
+    returnDate: Date,
+   // timeToGoTo: time
+   //timeToArrival: time
+   typeTransportId: number,
+   obervations: string
+}
