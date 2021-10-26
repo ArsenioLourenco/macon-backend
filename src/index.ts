37e4fa8a4bf68860f8a61e6f852a24cc44225806
Luis Caputo <luis.caputo@interdigitos.local>
2021-10-26 11:50:14 +0100

    :cookie: swagger docs setup was created

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+import "dotenv/config";
 import 'reflect-metadata';
 import cookieParser from 'cookie-parser';
+import swaggerJdocs from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 import express from "express";
-import dotenv from 'dotenv-safe';
+import dotenv from "dotenv-safe";
 import helmet from "helmet";
-import logger from 'morgan';
+import logger from "morgan";
 import http from "http"
 import cors from "cors";
 // Routes
@@ -27,12 +30,40 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// OpenApi||API documentation
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      version: "1.0.0",
+      title: "Macon-Api-Documentation",
+      description: "This is the endepoint docs from macon-backEnd || api",
+      contact: {
+        name: "Interdigitos LDA"
+      },
+      servers: ["http://localhost:6800"]
+    }
+  },
+  apis: ["./routes/users.routes.ts"]
+};
+const swaggerDocs = swaggerJdocs(
+  swaggerOptions
+);
+app.use("/api-docs/v1", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+/**
+ * @openapi
+ * /:
+ *   get:
+ *     description: Welcome to swagger-jsdoc!
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
 // Path configure
 app.use('/functionsPath', express.static('pathConf'));
 
 // Routes
 app.use(routes);
-
+// Server Configs
 const PORT = process.env.PORT || 6800;
 const HOST = '0.0.0.0';
 //Listen Port
