4b07ca1bbd25419ef72e7199f8ef65564d1a9a09
Rosa Chicato <rosacristinachicafo@gmail.com>
2021-11-04 18:32:32 +0100

    :lock: configurando getTravel

---
+++ src/controllers/travels/getTravels.controller.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { AppResponse } from "../../@types";
+import { Travels } from "../../models/Travels";
+import TravelsRepository from "../../repositories/travels.repository";
+
+
+// interface IGetTravel{
+//     originProvince:number,
+//      destinyProvince:number,
+//       departureDate:Date,
+//     returnDate?: Date
+
+// }
+
+export default class GetTravelsController{
+    async handle(request: Request, response: Response<AppResponse<Travels[]>>){
+        const {originProvince, destinyProvince, departureDate, returnDate}= request.body;
+        try{
+            const 
+                travelsRepository = getCustomRepository( TravelsRepository ),
+                getting = await travelsRepository.find( {where:{originProvince, destinyProvince, departureDate,}});
+                
+            if(getting){
+                console.log(originProvince, destinyProvince, departureDate)
+                return response.status(200)
+                    .json({ success:true, data: getting });
+            }
+else{
+                console.log("rosa")
+                return response.status(400)
+                .json({ success: false, message: 'Não Temos Essa Viagem Agendada.'});
+            }
+            
+        } catch(err){
+            return response.status(500)
+                .json({ success: false, message: err.message });
+        }
+    }
+}
--- src/routes/travels.routes.ts
+++ src/routes/travels.routes.ts
@@ -3,6 +3,7 @@ import CreateTravelsController from "../controllers/travels/createTravel.control
 import DeleteAllTravelController from "../controllers/travels/deleteAllTravel.controller"
 import DeleteTravelController from "../controllers/travels/deleteTravel.controller"
 import GetAllTravelsController from "../controllers/travels/getAllTravels.controller"
+import GetTravelsController from "../controllers/travels/getTravels.controller"
 import UpdateTravelsController from "../controllers/travels/updateTravels.controller"
  
 const router= Router();
@@ -12,11 +13,13 @@ const deleteTravelController = new DeleteTravelController();
 const getAllTravelController = new GetAllTravelsController();
 const deleteAllTravelController = new DeleteAllTravelController();
 const updateTravelsController = new UpdateTravelsController();
+const getTravelsController = new GetTravelsController();
 
 router.post("/travels/create", createTravelsController.handle);
 router.delete("/travels/delete/:id", deleteTravelController.handle);
 router.delete("/travels/all", deleteAllTravelController.handle);
 router.get("/travels/all", getAllTravelController.handle);
 router.put("/travels/update", updateTravelsController.handle);
+router.get("/travels", getTravelsController.handle )
 
 export default router
