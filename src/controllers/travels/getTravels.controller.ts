8762d7334cf936da51c32099b97ed3ab3ab38d45
Rosa Chicato <rosacristinachicafo@gmail.com>
2021-11-08 17:47:23 +0100

    :cookie: criando os serviços getTravel

--- src/controllers/travels/getTravels.controller.ts
+++ src/controllers/travels/getTravels.controller.ts
@@ -4,34 +4,52 @@ import { AppResponse } from "../../@types";
 import { Travels } from "../../models/Travels";
 import TravelsRepository from "../../repositories/travels.repository";
 
-export interface IGetTravel{
-    originProvince:number,
-    destinyProvince:number,
-    departureDate:Date,
+export interface IGetTravel {
+    originProvince: number,
+    destinyProvince: number,
+    departureDate: Date,
     returnDate?: Date
-
 }
 
-export default class GetTravelsController{
-    async handle(request: Request<IGetTravel>, response: Response<AppResponse<Travels[]>>){
-        const {originProvince, destinyProvince, departureDate, returnDate}= request.body;
-        try{
-            const 
-                travelsRepository = getCustomRepository( TravelsRepository ),
-                getting = await travelsRepository.find( {where:{originProvince, destinyProvince, departureDate}, relations: ['transport', 'originProvince', 'destinyProvince']});
-                
-            if(getting){
-                console.log(originProvince, destinyProvince, departureDate)
+export default class GetTravelsController {
+    async handle(request: Request<IGetTravel>, response: Response<AppResponse<Travels[]>>) {
+        try {
+            const { originProvince, destinyProvince, departureDate, returnDate } = request.params;
+            if (!returnDate) {
+                const
+                    travelsRepository = getCustomRepository(TravelsRepository),
+                    getting = await travelsRepository.find(
+                        { 
+                            where: { originProvince, destinyProvince, departureDate }, 
+                            relations: ['originProvince', 'destinyProvince'] 
+                        }
+                    );
+                if (getting.length != 0) {
+                    return response.status(200)
+                        .json({ success: true, data: getting });
+                }
+                else {
+                    return response.status(400)
+                        .json({ success: false, message: 'Não Temos Essa Viagem Agendada.' });
+                }
+            }
+            const
+                travelsRepository = getCustomRepository(TravelsRepository),
+                getting = await travelsRepository.find(
+                    { 
+                        where: { originProvince, destinyProvince, departureDate, returnDate }, 
+                        relations: ['originProvince', 'destinyProvince'] 
+                    }
+                );
+            if (getting.length <= 0) {
                 return response.status(200)
-                    .json({ success:true, data: getting });
+                    .json({ success: true, data: getting });
             }
-else{
-                console.log("rosa")
+            else {
                 return response.status(400)
-                .json({ success: false, message: 'Não Temos Essa Viagem Agendada.'});
+                    .json({ success: false, message: 'Não Temos Essa Viagem Agendada.' });
             }
-            
-        } catch(err){
+        } catch (err) {
             return response.status(500)
                 .json({ success: false, message: err.message });
         }
