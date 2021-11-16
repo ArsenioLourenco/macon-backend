b1d12f0f8a993309c14a1dcf1e596f802fbe0a91
Rosa Chicato <rosacristinachicafo@gmail.com>
2021-11-16 16:09:30 +0100

    :art: tratativas de erros no controller e no serviço createTransport

--- src/services/transport/createTransport.service.ts
+++ src/services/transport/createTransport.service.ts
@@ -1,54 +1,49 @@
+
 import { getCustomRepository } from "typeorm";
 import TransportRepository from "../../repositories/Transport";
 import TypeTransportRepository from "../../repositories/typeTransport.repository";
 
-export interface ICreateTransport{
+export interface ICreateTransport {
     transportName: string,
     transportNumber: number,
     totalPlace: number,
     typeTransport: number
 }
 
-export default class CreateTransport{
+export default class CreateTransport {
     async execute({
-        transportName, 
-        transportNumber, 
-        totalPlace, 
-        typeTransport } : ICreateTransport)
-    {
-        try{    
-            const 
-                transportRepository = getCustomRepository( TransportRepository ),
-                typeTransportRepository = getCustomRepository( TypeTransportRepository ),
-                alreadyExistsTransportNumber = await transportRepository.findOne( { where: { transportNumber } });
-
-            if(alreadyExistsTransportNumber){
-                return 'Não é permitido a duplicação de Autocarros';
-            }
-
-            const verifyIfExistTypeTransport = await typeTransportRepository.findOne( typeTransport );
-
-            if(!verifyIfExistTypeTransport){
-                return 'Não temos esse modelo de Autocarro!';
-            }
-
-            const createTransport = transportRepository.create({
-                transportName,
-                transportNumber,
-                totalPlace,
-                typeTransport: verifyIfExistTypeTransport
-            });
-
-            await transportRepository.save(
-                createTransport
-            );
-
-            return [createTransport, 'Autocarro Registrado.'];
+        transportName,
+        transportNumber,
+        totalPlace,
+        typeTransport }: ICreateTransport) {
+            console.log(" transport é: " , typeTransport)
+        try {
+            const
+                transportRepository = getCustomRepository(TransportRepository),
+                typeTransportRepository = getCustomRepository(TypeTransportRepository),
+                verifyIfExistTypeTransport = await typeTransportRepository.findOne({where:{id:typeTransport}});
+               
+                if (verifyIfExistTypeTransport) {
+                    const createTransport = transportRepository.create({
+                        transportName,
+                        transportNumber,
+                        totalPlace,
+                        typeTransport: verifyIfExistTypeTransport
+                    });
+        
+                    await transportRepository.save(
+                        createTransport
+                    );
+        
+                    return [createTransport, 'Autocarro Registrado.'];
+                }
+    
+        
         }
-        catch(err){
+        catch (err) {
             return err.message;
         }
-    } 
+    }
 }
 
 
