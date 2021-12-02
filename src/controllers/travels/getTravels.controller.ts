65ef28194a8b974c1134e698350b89ec33690068
Rosa Chicato <rosacristinachicafo@gmail.com>
2021-12-02 11:04:33 +0100

    adicionando o return no getTravel

--- src/controllers/travels/getTravels.controller.ts
+++ src/controllers/travels/getTravels.controller.ts
@@ -18,7 +18,6 @@ export default class GetTravelsController {
 
             if (departureDate) {
                 const partida = departureDate.toLocaleString().split("-")
-                console.log("today", today, "partida", partida)
                 if (today[2] >= partida[0] && today[1] > partida[1] && today[0] <= partida[2] || today[2] >= partida[0] && today[1] == partida[1] && today[0] > partida[2]) {
                     return response.status(400).json({ success: false, message: 'essa viagem já ocorreu' })
                 }
